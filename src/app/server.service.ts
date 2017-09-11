42e4f74f3d53636e8288c7cc00fa70029b720b31
Daniel Taylor <Daniel Taylor>
2017-09-10 20:40:05 -0600

    Sending Get Request to FireBase

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class ServerService {
     constructor(private http: Http) {  }
 
     storeServers(servers: any[]){
-        return this.http.post('https://http-angular-project.firebaseio.com/data.json', servers);
+        const header = new Headers({
+            'ContentType': 'application/json'            
+        })
+        return this.http.post('https://http-angular-project.firebaseio.com/data.json',
+         servers,
+         {headers: header });
+    }
+
+    getServers(){
+        return this.http.get('https://http-angular-project.firebaseio.com/data.json');
     }
 }
